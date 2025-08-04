aae4b51fe5efbcc6a4651c47db761730ad6ef9ce
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 11:17:31 -0300

    feat -  Definicndo variáveis do tipo string e object e dynamic

---
+++ CSFundamentos1/TipoDeDados4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Definicndo variáveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso C#";
+String titulo = "Curso C# Essencial";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isto é uma string";
+valor = "Isto é uma string alterada";
+valor = "teste";//cada vez que é atribuido um valor é reservado um novo local na memória
+
+//StringBuilder
+
+object nota = 10;
+object valor1 = 8.55m;
+object nome1 = "Maria";
+object ativa = true;
+object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+
+Console.ReadLine();
