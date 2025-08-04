14c807fdf9d41e03c30adcb7b1a75341955bef81
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 13:21:47 -0300

    feat - exercicios

---
+++ CSFundamentos1/Exercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿// 1- Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+int idade = 35;
+System.Console.WriteLine(idade);
+
+// 2- Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+string nome = "Maria";
+System.Console.WriteLine(nome);
+
+// 3- Crie uma variável chamada altura e atribua a ela o valor 3.45.
+double altura = 3.45;
+System.Console.WriteLine(altura);
+
+// 4- Crie uma variável chamada data e atribua a ela a data 04/09/1999 e exiba o seu valor.
+DateTime data = new(1999,09,04);
+System.Console.WriteLine(data);
+
+
+// Exercícios fase 1
+// 5- Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor;
+const int ANO = 12;
+System.Console.WriteLine(ANO);
+
+// 6- Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o
+// valor 7.80 e exiba o seu valor.
+double? numero1 = 7.80d;
+System.Console.WriteLine(numero1);
+
+// 9  - Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+// seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+// operação de soma)
+int x = 77;
+int y = 66;
+
+System.Console.WriteLine($"o resutlado é {x + y}");
+
