07da42bf1cba161f4c6217ae830f3179c3985582
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 13:39:23 -0300

    feat - Saida de dados: Usando sequência de dados

---
+++ CSFundamentos1/SaidaDeDados2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+System.Console.WriteLine("Saida de dados: Usando sequência de dados");
+
+System.Console.WriteLine(" \a  sinal sonoro");
+System.Console.WriteLine(" \b  backspace");
+System.Console.WriteLine(" \f  alimentação de formulario");
+System.Console.WriteLine(" \n  Nova linha");
+System.Console.WriteLine(" \r  Carriage return");
+System.Console.WriteLine(" \t  Tabulação horizontal");
+System.Console.WriteLine(" \v  Tabulação vertical");
+System.Console.WriteLine(" \'  aspas simples");
+System.Console.WriteLine(" \"  aspas duplas");
+System.Console.WriteLine(" \\  barra invertida");
