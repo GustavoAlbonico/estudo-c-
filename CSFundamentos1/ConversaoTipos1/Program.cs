7fc42e002bbc454ec0b9801b68581bad5c587602
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 13:52:58 -0300

    feat - Conversao de tipos 1

---
+++ CSFundamentos1/ConversaoTipos1/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("COnversao de tipos");
+
+
+//conversao implcita
+int varInt = 100;
+double varDouble = varInt;
+
+System.Console.WriteLine(varDouble);
+int numeroInt = 12321;
+long numeroLong = numeroInt;
+long numeroFloat = numeroInt;
+long numeroDouble = numeroInt;
+long numeroDecimal = numeroInt;
+
+System.Console.WriteLine(numeroInt);
+System.Console.WriteLine(numeroLong);
+System.Console.WriteLine(numeroFloat);
+System.Console.WriteLine(numeroDouble);
+System.Console.WriteLine(numeroDecimal);
+
+//coversao explicita 
+
+double varDouble1 = 12.456;
+int varInt1 = (int)varDouble1;
+
+System.Console.WriteLine(varInt1);
