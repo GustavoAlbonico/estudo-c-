8ca72b103e641fe5ce28785554890fa1f591a5f3
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 11:49:50 -0300

    feat - Nullable Types

---
+++ CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Nullable Types");
+
+Nullable<bool> b = null;
+int? i = null;
+double? d = null;
+
+System.Console.WriteLine(i);
+System.Console.WriteLine(b);
+System.Console.WriteLine(d);
+
+int? a = null;
+int ba = a ?? 0;
+
+System.Console.WriteLine(ba);
+
+if (a.HasValue)
+{
+    System.Console.WriteLine($"b = {a.Value}");
+}
+else
+{
+    System.Console.WriteLine("a não possui um valor (null)");
+}
