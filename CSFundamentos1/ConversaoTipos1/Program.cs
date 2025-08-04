ab50d03b0a9ea51c5c76a129280d8896dadf3e12
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-04 14:13:42 -0300

    feat - tipos de dados2

--- CSFundamentos1/ConversaoTipos1/Program.cs
+++ CSFundamentos1/ConversaoTipos1/Program.cs
@@ -1,26 +1,52 @@
-﻿System.Console.WriteLine("COnversao de tipos");
+﻿Console.WriteLine("COnversao de tipos");
 
 
 //conversao implcita
 int varInt = 100;
 double varDouble = varInt;
 
-System.Console.WriteLine(varDouble);
+Console.WriteLine(varDouble);
 int numeroInt = 12321;
 long numeroLong = numeroInt;
 long numeroFloat = numeroInt;
 long numeroDouble = numeroInt;
 long numeroDecimal = numeroInt;
 
-System.Console.WriteLine(numeroInt);
-System.Console.WriteLine(numeroLong);
-System.Console.WriteLine(numeroFloat);
-System.Console.WriteLine(numeroDouble);
-System.Console.WriteLine(numeroDecimal);
+Console.WriteLine(numeroInt);
+Console.WriteLine(numeroLong);
+Console.WriteLine(numeroFloat);
+Console.WriteLine(numeroDouble);
+Console.WriteLine(numeroDecimal);
 
 //coversao explicita 
 
 double varDouble1 = 12.456;
 int varInt1 = (int)varDouble1;
 
-System.Console.WriteLine(varInt1);
+Console.WriteLine(varInt1);
+
+//---------------//
+int valorInt2 = 123;
+double valorDouble2 = 12.45;
+decimal valorDecimal2 = 12.8548m;
+
+string s1 = valorInt2.ToString();
+string s2 = valorDouble2.ToString();
+string s3 = valorDecimal2.ToString();
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+
+//------  Convert class ---------// 
+//quando houver erro vai dispara uma exception
+
+int valorInt3 = 123;
+double valorDouble3 = 12.45;
+decimal valorDecimal3 = 12.8548m;
+
+
+Console.WriteLine(Convert.ToString(valorInt3));
+Console.WriteLine(Convert.ToDouble(valorInt3));
+Console.WriteLine(Convert.ToString(valorDouble3));
+Console.WriteLine(Convert.ToInt32(valorDecimal3));
