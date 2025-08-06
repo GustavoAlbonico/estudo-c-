dc06eb58f16c73eb0d75383e1fd684336affc527
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-06 12:58:35 -0300

    feat - metodos

---
+++ ClassesMetodos/Metodos/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Métodos");
+
+MinhaClasse minhaClasse = new();
+minhaClasse.Saudacao();
+minhaClasse.SaudacaoParametro("Jose", DateTime.Now.ToShortDateString());
+
+class MinhaClasse
+{
+    public void Saudacao()
+    {
+        System.Console.WriteLine("Bem-vindo");
+        ExibirDataAtual();
+    }
+
+    public void ExibirDataAtual() => System.Console.WriteLine(DateTime.Now.ToShortDateString());
+
+    public void SaudacaoParametro(string nome, string data)
+    {
+        System.Console.WriteLine(nome);
+        System.Console.WriteLine(data);
+    }
+}
