8f9b1c01e479b69f8c1a60f618b24c4c17bf235c
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-06 13:48:02 -0300

    feat - construtor

---
+++ ClassesMetodos/Construtor/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Construtor");
+
+Aluno aluno = new("Roberto", 30, "Masculino", "S");
+
+System.Console.WriteLine(aluno.Nome ?? "null");
+System.Console.WriteLine(aluno.Idade);
+System.Console.WriteLine(aluno.Sexo ?? "null");
+System.Console.WriteLine(aluno.Aprovado ?? "null");
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+
+    public Aluno(string nome) => Nome = nome;
+
+    public Aluno(string nome, int idade, string sexo, string aprovado) : this(nome)
+    {
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+}
