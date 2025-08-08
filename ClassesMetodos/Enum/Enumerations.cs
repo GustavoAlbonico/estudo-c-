1c9da00b1f38893a899a18334c2bc5696ab646e2
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-08 19:07:17 -0300

    feat - Enum

---
+++ ClassesMetodos/Enum/Enumerations.cs
@@ -0,0 +1,45 @@
+namespace Enum;
+
+
+enum DiasDaSemana
+{
+    Segunda, //0
+    Terça, //1
+    Quarta, //2
+    Quinta, //3
+    Sexta, //4
+    Sábado, //5
+    Domingo //6
+}
+
+enum Categorias
+{
+    Moda,
+    Automotivo,
+    Artes,
+    Bebidas,
+    Livros,
+    Brinquedos
+}
+
+// enum DiasDaSemana
+// {
+//     Segunda, //0
+//     Terça, //1
+//     Quarta, //2
+//     Quinta = 4,
+//     Sexta, //5
+//     Sábado, //6
+//     Domingo //7
+// }
+
+// enum DiasDaSemana
+// {
+//     Segunda = 0,
+//     Terça = 1, 
+//     Quarta = 3,
+//     Quinta = 4,
+//     Sexta = 15, 
+//     Sábado = 20, 
+//     Domingo = 30
+// }
---
+++ ClassesMetodos/Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enum;
+
+Console.WriteLine("Enum \n");
+
+Console.WriteLine(DiasDaSemana.Domingo);
+Console.WriteLine(DiasDaSemana.Segunda);
+
+int domingoOrdinal = (int)DiasDaSemana.Domingo;
+int segundaOrdinal = (int)DiasDaSemana.Segunda;
+
+Console.WriteLine($"{DiasDaSemana.Domingo} vale: {domingoOrdinal}");
+Console.WriteLine($"{DiasDaSemana.Segunda} vale: {segundaOrdinal}");
+
+Console.WriteLine($"{Categorias.Moda} - {(int)Categorias.Moda}");
+Console.WriteLine($"{Categorias.Automotivo} - {(int)Categorias.Automotivo}");
+Console.WriteLine($"{Categorias.Artes} - {(int)Categorias.Artes}");
+Console.WriteLine($"{Categorias.Bebidas} - {(int)Categorias.Bebidas}");
+Console.WriteLine($"{Categorias.Livros} - {(int)Categorias.Livros}");
+Console.WriteLine($"{Categorias.Brinquedos} - {(int)Categorias.Brinquedos}");
+
+System.Console.WriteLine("\n Selecione a Categoria teclando o seu valor");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+var nomeMembroEnum = (Categorias)valor;
+
+System.Console.WriteLine($"Voçe selecionou a categoria: {nomeMembroEnum}");
+
+
