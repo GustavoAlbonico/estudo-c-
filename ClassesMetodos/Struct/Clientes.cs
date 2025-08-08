f2f6b1304992370d78c275a05734f64b3b052697
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-08 18:49:58 -0300

    feat - Struct

---
+++ ClassesMetodos/Struct/Clientes.cs
@@ -0,0 +1,13 @@
+
+//não cria referencia na memoria
+public struct Cliente
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
---
+++ ClassesMetodos/Struct/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Struct \n");
+
+Cliente cliente = new();
+cliente.Nome = "Maria";
+cliente.Idade = 19;
+
+Console.WriteLine($"{cliente.Nome} {cliente.Idade}");
