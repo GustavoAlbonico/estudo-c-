76d9635085c62ec9964a9f7faf753af65641f9d8
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-12 17:37:56 -0300

    feat - Herança abstract

---
+++ POO/HerancaAbstract/Program.cs
@@ -0,0 +1,19 @@
+﻿//Classe abstrata não pode ser instanciada
+//métodos abstratos na herança obrigatóriamente precisam ser implementados na classe que tem herança
+
+Quadrado q = new Quadrado();
+Console.WriteLine(q.Descricao());
+
+Console.WriteLine("\nInforme a cor da figura");
+q.Cor = Console.ReadLine();
+
+Console.WriteLine("\nInforme o valor do lado do quadrado (m)");
+q.Lado = Convert.ToInt32(Console.ReadLine());
+
+q.CalcularArea();
+q.CalcularPerimetro();
+
+Console.WriteLine($"\nA área do quadrado é : {q.Area} m2");
+Console.WriteLine($"\nO perímetro do quadrado é {q.Perimetro} m "); 
+Console.WriteLine($"\nO quadrado tem a cor : {q.Cor}");
+Console.ReadKey();
---
+++ POO/HerancaAbstract/Quadrado.cs
@@ -0,0 +1,13 @@
+public class Quadrado : Forma
+{
+    public double Lado { get; set; }
+    public override void CalcularArea()
+    {
+        Area = Lado * Lado;
+    }
+
+    public override void CalcularPerimetro()
+    {
+        Perimetro = 4 * Lado;
+    }
+}
