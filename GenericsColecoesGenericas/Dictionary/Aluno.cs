db4df2de0b01c68ce27dc2e84073b75380d8bb54
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-13 11:47:08 -0300

    feat - Dictionary

---
+++ GenericsColecoesGenericas/Dictionary/Aluno.cs
@@ -0,0 +1,13 @@
+public class Aluno
+{
+    public Aluno(string? nome, int nota)
+    {
+        Nome = nome;
+        Nota = nota;
+    }
+
+    public string? Nome { get; set; }
+    public int Nota { get; set; }
+
+
+}
