fe2384da8f6ce0e60d2fd49e2d42cfd5fc4f3a93
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-14 17:45:25 -0300

    feat LINQ consulta | agrupamento

---
+++ DelegateExpressaoLambdaEventosLINQ/LINQAgrupamento1/Program.cs
@@ -0,0 +1,30 @@
+﻿var listaProdutos = Produto.GetProdutos();
+
+var produtosPorCategoria = listaProdutos.GroupBy(p => p.Categoria);
+
+// var produtosPorCategoria = listaProdutos
+//                             .GroupBy(p => p.Categoria)
+//                             .OrderBy(c => c.Key)
+//                             .Select(g => new
+//                             {
+//                                 Categoria = g.Key,
+//                                 Produtos = g.OrderBy(p => p.Nome)
+//                                 .Select(p => new
+//                                 {
+//                                     Nome = p.Nome,
+//                                     Preco = p.Preco,
+//                                     Estoque = p.Estoque,
+//                                 })
+//                             });
+
+foreach (var grupo in produtosPorCategoria)
+{
+    Console.WriteLine(grupo.Key + " : " + grupo.Count());
+
+    foreach (var produto in grupo)
+    {
+        Console.WriteLine($"  {produto.Nome} \t{produto.Preco:C2} \t{produto.Estoque}");
+    }
+}
+
+Console.ReadKey();
