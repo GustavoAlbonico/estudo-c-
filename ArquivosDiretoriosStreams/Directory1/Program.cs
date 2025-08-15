6883d23bd9ae09e0215efd387618766ae1798fe6
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-15 12:53:06 -0300

    feat - directory, file e stream

---
+++ ArquivosDiretoriosStreams/Directory1/Program.cs
@@ -0,0 +1,36 @@
+﻿var caminhoDiretorio = @"d:\dados\MeuDiretorio";
+string caminhoDestino = @"d:\dados\MeuDiretorioNovo";
+
+try
+{
+    if (Directory.Exists(caminhoDiretorio))
+    {
+        // Directory.CreateDirectory(caminhoDiretorio);
+
+        // Directory.Delete(caminhoDiretorio);
+
+        // string[] subdiretorios = Directory.GetDirectories(caminhoDiretorio);
+        // string[] subdiretorios = Directory.GetDirectories(@"d:\","p*");//iniciam com a letra p
+        // foreach (var subdir in subdiretorios)
+        //     Console.WriteLine(subdir);
+
+        // string[] arquivos = Directory.GetFiles(caminhoDestino);
+        // foreach (var arquivo in arquivos)
+        //     Console.WriteLine(arquivo);
+
+        Directory.Move(caminhoDiretorio, caminhoDestino);//como se tivese renomeado
+
+        Console.WriteLine("Diretório renomeado com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine($"O diretório  {caminhoDiretorio} não existe!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    //Console.WriteLine(ex.StackTrace);
+}
+
+Console.ReadKey();
---
+++ ArquivosDiretoriosStreams/FileStreamBlocoUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿string caminhoArquivo = @"d:\dados\poesia.txt";
+
+try
+{
+
+    //using é utilizado para ele fechar os recursos automaticamente depois de ler ele automaticamente
+    //pq o garbage colec tor não faz esse papel nesse caso
+
+    //using FileStream fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    using StreamReader leitor = File.OpenText(caminhoArquivo);//new StreamReader(fs); 
+
+    string? linha;
+    while ((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
