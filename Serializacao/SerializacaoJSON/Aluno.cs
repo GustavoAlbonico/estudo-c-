3b898b14ba7493caf53aa45e54fd0ef68e6cde53
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-15 13:30:46 -0300

    feat - Serialização

---
+++ Serializacao/SerializacaoJSON/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SerializacaoJSON;
+public class Aluno
+{
+    public int Id { get; set; }
+    
+    [JsonIgnore]
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Idade { get; set; }
+    public Aluno()//precisa ter esse construtor padrão
+    {
+    }
+    public Aluno(int id, string name, string email, int idade)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Idade = idade;
+    }
+}
---
+++ Serializacao/SerializacaoXML/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace SerializacaoXML;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Idade;
+
+    public Aluno()//precisa ter esse construtor padrão
+    {
+    }
+    public Aluno(int id, string name, string email, int idade)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Idade = idade;
+    }
+}
