27fb8d6fc1de7ccb307a1bac4e5887637dc8edde
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-18 17:03:18 -0300

    feat - SemaphoreSlim

---
+++ ProgramacaoAssincrona/SemaphoreSlim/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+SemaphoreSlim semaphoreSlim = new(4);
+
+for (int i = 0; i < 6; i++)
+{
+    string threadName = "Thread" + i;
+    int espera = 2 + 2 * i;
+
+    var thread = new Thread(() => AcessarBancoDados(threadName, espera));
+
+    thread.Start();
+}
+
+void AcessarBancoDados(string nome, int seconds)
+{
+    Console.WriteLine($"{nome} aguarda para acessar o banco de dados...");
+    semaphoreSlim.Wait();
+
+    Console.WriteLine($"{nome} foi autorizado para acessar o banco de dados...");
+    Thread.Sleep(TimeSpan.FromSeconds(seconds));
+
+    Console.WriteLine($"{nome} foi concluido");
+    semaphoreSlim.Release();
+    
+}
