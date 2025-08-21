ece43e98621d17cb70a9a484b904aeb6742b60fa
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-20 22:31:15 -0300

    feat - Expressoes colecoes | Using tipos adicionais

---
+++ Extras/ExpressoesColecoes_C12/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numeros = [1, 2, 3, 4, 5];
+Span<string> span = ["asdasd", "sadasdasd", "asdasdas"];
+ReadOnlySpan<string> readOnlySpan = ["asdasd", "sadasdasd", "asdasdas"];
+
+List<int> lista1 = [1, 2, 3];
+List<int> lista2 = [4, 5, 6];
+List<int> listaCombinada = [.. lista1, .. lista2];
+
+
+// VALOR PADRÃO LAMBDA
+var incrementarValor = (int origem, int incremento = 2) => origem + incremento;
---
+++ Extras/UsingTiposAdicionais_C12/Program.cs
@@ -0,0 +1,15 @@
+﻿using Transacao = (decimal Valor, System.DateTime Data, TipoTransacao Tipo);
+
+List<Transacao> transacoes = new List<Transacao>
+{
+    (100.00M, DateTime.Now, TipoTransacao.Deposito),
+    (100.00M, DateTime.Now, TipoTransacao.Saque),
+    (100.00M, DateTime.Now, TipoTransacao.Transferencia),
+    (100.00M, DateTime.Now, TipoTransacao.Deposito),
+};
+public enum TipoTransacao
+{
+    Deposito,
+    Saque,
+    Transferencia
+}
