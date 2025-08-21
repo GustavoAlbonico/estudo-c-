a09fc151ea4f710eeca289404f4085aa8c70cd84
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-21 11:33:18 -0300

    feat - DateOnly | TimeOnly

---
+++ Extras/RawStringLiteral_C11/Program.cs
@@ -0,0 +1,15 @@
+﻿var literal1 = """ asdasdasd "teste literal" """;
+System.Console.WriteLine(literal1);
+
+var nome = "Gustavo";
+var pais = "Brasil";
+
+var literal2 = $$"""
+                {
+                    "nome": "{{nome}}"   
+                    "pais": "{{pais}}"   
+                }
+               """;
+
+
+var literal3 = $""" Ola meu nome é {nome} """;
